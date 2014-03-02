76dd0976e4c7e8fca3da6359e201f9f454b1667f
Edward Look <edwlook@gmail.com>
2014-03-01 16:00:57 -0800

    Fix indentations

--- client/www/js/index.js
+++ client/www/js/index.js
@@ -17,36 +17,35 @@
  * under the License.
  */
 var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicity call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        console.log('Received Event: ' + id);
-        app.initializeMap();
-    },
-    initializeMap: function() {
-        var mapOptions = {
-            zoom: 14,
-            center: new google.maps.LatLng(37.871076, -122.2594582)
-        };
-        map = new google.maps.Map(document.getElementById("map-canvas"),
-            mapOptions);
-    }
+  // Application Constructor
+  initialize: function() {
+    this.bindEvents();
+  },
+  // Bind Event Listeners
+  //
+  // Bind any events that are required on startup. Common events are:
+  // 'load', 'deviceready', 'offline', and 'online'.
+  bindEvents: function() {
+    document.addEventListener('deviceready', this.onDeviceReady, false);
+  },
+  // deviceready Event Handler
+  //
+  // The scope of 'this' is the event. In order to call the 'receivedEvent'
+  // function, we must explicity call 'app.receivedEvent(...);'
+  onDeviceReady: function() {
+    app.receivedEvent('deviceready');
+  },
+  // Update DOM on a Received Event
+  receivedEvent: function(id) {
+    var parentElement = document.getElementById(id);
+    console.log('Received Event: ' + id);
+    app.initializeMap();
+  },
+  initializeMap: function() {
+    var mapOptions = {
+      zoom: 14,
+      center: new google.maps.LatLng(37.871076, -122.2594582)
+    };
+    map = new google.maps.Map(document.getElementById("map-canvas"), mapOptions);
+  }
 };
