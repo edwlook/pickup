04b1b7a32b232ca2171d7e865aee802c286fa97e
Edward Look <edwlook@gmail.com>
2014-03-02 01:50:58 -0800

    Add CORS support

--- server/routes.js
+++ server/routes.js
@@ -5,6 +5,12 @@ var events = require('./controllers/events'),
 
 module.exports = function(app) {
 
+  app.all('*', function(req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "X-Requested-With");
+    next();
+   });
+
   app.get('/events', events.getAll);
 
   app.get('/events/:id', events.getOne);
